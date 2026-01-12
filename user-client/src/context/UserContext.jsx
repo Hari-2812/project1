9494d79e908200472b31d965222af528326fcc7a
Hari-2812 <v.hari2812@gmail.com>
2026-01-12 23:04:21 +0530

    Checkout page form created with required

---
+++ user-client/src/context/UserContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  /* LOAD USER FROM LOCAL STORAGE */
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  /* LOGIN */
+  const login = (userData) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+    localStorage.setItem("token", "logged-in");
+  };
+
+  /* LOGOUT */
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <UserContext.Provider value={{ user, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
