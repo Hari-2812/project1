f94c69535dc1858a36bafff6f4ce4e5e832c03e1
Guru <gurudayaluday2003@gmail.com>
2026-01-14 00:30:53 +0530

    completed marquee and new arrival section

--- user-client/src/pages/Home.jsx
+++ user-client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import HeroBanner from "../components/HeroBanner";
+import Marquee from "../components/Marquee";
+import NewArrivals from "../components/NewArrivals";
 
 export default function Home() {
   return (
     <div className="home-page">
       <HeroBanner />
+      <Marquee /> {/* 👈 Announcement bar */}
+      <NewArrivals />
       {/* Add more sections here later */}
     </div>
   );
